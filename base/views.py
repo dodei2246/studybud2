79263593faa6176c97d7be214905ba861520917e
dodei2246 <danielodei2@gmail.com>
2024-09-17 17:20:04 -0400

    3:43
    User profile page

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.db.models import Q
+from .models import Message
 from django.contrib.auth.decorators import login_required 
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -66,12 +67,15 @@ def home(request):
     )
     topics = Topic. objects.all()
     room_count = rooms.count()
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    room_messages= Message.objects.filter(Q(room__topic__name__icontains=q))
+
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages':room_messages}
     return render(request, 'base/home.html',context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
     participants = room.participants.all()
     if request.method == 'POST':
         message = Message.objects.create(
@@ -84,6 +88,12 @@ def room(request, pk):
 
     context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'room.html', context)
+
+def userProfile(request,pk):
+    user= User.objects.get(id=pk)
+    rooms= user.room_set.all()
+    context={'user':user, 'rooms':rooms}
+    return render(request, 'base/profile.html', context)
 
 @login_required(login_url="login")
 def createRoom(request):
